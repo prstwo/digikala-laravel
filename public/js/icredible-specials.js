45e38495da71a919177964d56217b0929ae642a4
unknown <prs2.pari@gmail.com>
2021-04-20 17:17:25 +0430

    some bugs eliminated

--- public/js/icredible-specials.js
+++ public/js/icredible-specials.js
@@ -2,28 +2,28 @@
 this file is used for sliders functionality (4 slides)
 */
 document.querySelector('.arrow-right-2').addEventListener('click', function(){
-    moveSlides(-4);
+    moveSlides2(-4);
 });
 document.querySelector('.arrow-left-2').addEventListener('click', function(){
-    moveSlides(4);
+    moveSlides2(4);
 });
 
-let productNumber=4;
-function showSlides(n){
+let productCount=4;
+function showSlides2(n){
     let i;
     let products = document.querySelectorAll(".product-item-container-3");
-    if (n > products.length) {productNumber = 4}    
-    if (n < 1) {productNumber = products.length}
+    if (n > products.length) {productCount = 4}
+    if (n < 1) {productCount = products.length}
     for (i = 0; i < products.length; i++) {
-        products[i].style.display = "none"; 
+        products[i].style.display = "none";
     }
-    products[productNumber-1].style.display = "block";  
-    products[productNumber-2].style.display = "block";  
-    products[productNumber-3].style.display = "block";  
-    products[productNumber-4].style.display = "block";  
+    products[productCount-1].style.display = "block";
+    products[productCount-2].style.display = "block";
+    products[productCount-3].style.display = "block";
+    products[productCount-4].style.display = "block";
 }
-function moveSlides(n){
-    showSlides(productNumber+=n);
+function moveSlides2(n){
+    showSlides2(productCount+=n);
 }
 
-showSlides(productNumber);
+showSlides2(productCount);
--- public/js/nav-menu.js
+++ public/js/nav-menu.js
@@ -2,124 +2,132 @@
          navlink.addEventListener('mouseover',function(e){
             e.target.nextElementSibling.classList.add('show');
             document.querySelector('.overlay').classList.add('active')
-            
+
         })
          navlink.addEventListener('mouseup',function(el){
              if(el.target!= document.querySelector('.submenu')&& el.target!= document.querySelectorAll('.nav-link-title')){
-                el.target.nextElementSibling.classList.remove('show');  
+                el.target.nextElementSibling.classList.remove('show');
 
              }
-           
-        });  */  
 
-document.querySelector('.nav-container').addEventListener('mouseover',function(e){        
+        });  */
+
+document.querySelector('.nav-container').addEventListener('click',function(e){
     if(e.target.classList.contains('toggle')){
-                let linktitle=e.target;
-                document.querySelector('.overlay').classList.add('active');
-                linktitle.nextElementSibling.classList.add('show');
-            }
+        let linkTitle=e.target;
+        document.querySelector('.overlay').classList.toggle('active');
+        let elem=linkTitle.nextElementSibling;
+
+        elem.classList.toggle('show');
+        if(linkTitle.nextElementSibling.classList.contains('show')){
+            document.querySelector('.overlay').addEventListener('click', function(e){
+                if(!elem.contains(e.target)){
+                    elem.classList.remove('show');
+                    document.querySelector('.overlay').classList.remove('active');
+
+                }
+            })
+        }
+    }
 });
 
+/*
 let submenus = document.querySelectorAll('.submenu');
 for(var i=0;i<submenus.length;i++){
     submenus[i].addEventListener('mouseleave',(e2)=>{
         e2.target.classList.remove('show');
         document.querySelector('.overlay').classList.remove('active');
-                })   
+                })
         submenus[i].addEventListener('mouseover',(e4)=>{
         e4.target.classList.add('show');
         document.querySelector('.overlay').classList.add('active');
-                })  
+                })
 }
 let toggles = document.querySelectorAll('.toggle');
 for(var j=0;j<toggles.length;j++){
     var current = toggles[j];
-    toggles[j].addEventListener('mouseleave',(e)=>{   
+    toggles[j].addEventListener('mouseleave',(e)=>{
     console.log(current.nextElementSibling);
-    //bebinam chera in ba inke roye title khodeshe, digivlub ro pass mide
-        /* if(e.target!=document.getElementsByClassName('submenu')[j]){
-            document.getElementsByClassName('submenu')[j].classList.remove('show');
-            document.querySelector('.overlay').classList.remove('active');
-        } */
-       
-    })   
-}
 
-                      
-            
-           /*  e.target.nextElementSibling.classList.add('show');
-            document.querySelector('.overlay').classList.add('active')
-        )
-        .forEach((navlink)=>{
-            navlink.addEventListener('mouseover',function(e){
-               e.target.nextElementSibling.classList.add('show');
-               document.querySelector('.overlay').classList.add('active')
-               
-           })
-            navlink.addEventListener('mouseup',function(el){
-                if(el.target!= document.querySelector('.submenu')&& el.target!= document.querySelectorAll('.nav-link-title')){
-                   el.target.nextElementSibling.classList.remove('show');  
-   
-                }
-              
-           }); 
- */
+
+    })
+} */
 
 
 
-        //in script temporary hast
-       /*  navlinks.addEventListener('click',function(e){
-            let link=e.target;
-            if(link.classList.contains('toggle')){
-                link.nextElementSibling.classList.toggle('show');
-             }
-        }) 
+/*  e.target.nextElementSibling.classList.add('show');
+ document.querySelector('.overlay').classList.add('active')
+)
+.forEach((navlink)=>{
+ navlink.addEventListener('mouseover',function(e){
+    e.target.nextElementSibling.classList.add('show');
+    document.querySelector('.overlay').classList.add('active')
+
+})
+ navlink.addEventListener('mouseup',function(el){
+     if(el.target!= document.querySelector('.submenu')&& el.target!= document.querySelectorAll('.nav-link-title')){
+        el.target.nextElementSibling.classList.remove('show');
+
+     }
+
+});
+*/
+
+
+
+//in script temporary hast
+/*  navlinks.addEventListener('click',function(e){
+     let link=e.target;
+     if(link.classList.contains('toggle')){
+         link.nextElementSibling.classList.toggle('show');
+      }
+ })
 })*/
 
 let due= new Date(2021, 7, 24, 15,30,0);
 
-/* 
+/*
     setInterval(()=>{
-        let now= new Date(); 
+        let now= new Date();
         let ellipse=Math.abs(due-now);
         let difference = (ellipse - due.toMillieSeconds) ;
         console.log(ellipse)
     }, 1000); */
-   var second =60;
-   var minute = 60;
-   var hour=24;
-   var day = 60;
-   let dayDiv= document.querySelector('.counter-day');
-   let hourDiv= document.querySelector('.counter-hour');
-   let minuteDiv= document.querySelector('.counter-minute');
-   let secondDiv= document.querySelector('.counter-second');
-            setInterval(function(){    
-            //console.log(`${day}:${hour}:${minute}:${second-=1}-------`);   
-            secondDiv.innerText=second--;
+var second =60;
+var minute = 60;
+var hour=24;
+var day = 60;
+let dayDiv= document.querySelector('.counter-day');
+let hourDiv= document.querySelector('.counter-hour');
+let minuteDiv= document.querySelector('.counter-minute');
+let secondDiv= document.querySelector('.counter-second');
+setInterval(function(){
+    //console.log(`${day}:${hour}:${minute}:${second-=1}-------`);
+    secondDiv.innerText=second--;
+    hourDiv.innerText=hour;
+    dayDiv.innerText=day;
+    minuteDiv.innerText=minute;
+
+    if(second<=0){
+        second=60;
+        minute-=1;
+        minuteDiv.innerText=minute;
+
+        if(minute<=0){
+            minute=60;
+            hour-=1;
             hourDiv.innerText=hour;
-            dayDiv.innerText=day;
-            minuteDiv.innerText=minute;
 
-            if(second<=0){
-                second=60;
-                minute-=1;
-                minuteDiv.innerText=minute;
 
-                if(minute<=0){
-                    minute=60;
-                    hour-=1;
-                   hourDiv.innerText=hour;
+            if(hour<=0){
+                hour=24;
 
-                    
-                    if(hour<=0){
-                        hour=24;
-                        
-                        day-=1;
-                        dayDiv.innerText=day;
-                    }
-                }
-            } 
-        }, 1000);
-         
-        
+                day-=1;
+                dayDiv.innerText=day;
+            }
+        }
+    }
+}, 1000);
+
+
 
