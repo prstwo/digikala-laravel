2aa2d88279633bf005363020cc3d81744fec702d
unknown <prs2.pari@gmail.com>
2021-04-25 17:26:36 +0430

    some items are added to sandwich menu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,8 +24,80 @@
 
 </head>
 <body>
+<?php
+$sandwichMenuItemsP1 = [
+    ['class'=>'"fa fa-list-ul"', 'title'=>'دسته بندی کالاها'],
+    ['class'=>'"fa fa-shopping-basket"', 'title'=>'سوپرمارکت'],
+    ['class'=>'"fa fa-percent"', 'title'=>' تخفیف ها و پشنهادها'],
+    ['class'=>'"fa fa-check-circle-o"', 'title'=>'  دیجی کالای من']
+];
+
+$sandwichMenuItemsP2 = [
+    ['class'=>'"fa fa-random"', 'title'=>'دیجی کلاب'],
+    ['class'=>'"fa fa-paypal"', 'title'=>'دیجی پی'],
+    ['class'=>'""', 'title'=>'سوالی دارید؟'],
+    ['class'=>'""', 'title'=>'فروشنده شوید']
+];
+$productItemContainer3 = [
+    ['img'=>'"images/phone-1.jpg"','product-title'=>'گوشی موبایل شیائومی مدل POCO X3 M2007J20CG دو سیم‌ کارت ظرفیت 128 گیگابایت
+                                            گوشی موبایل شیائومی مدل POCO X3 M2007J20CG دو سیم‌ کارت ظرفیت 128 گیگابایت', 'old-price'=>'۸,۰۰۰,۰۰۰',
+        'discount'=>' ۱۵٪', 'new-price'=>'۷,۲۲۹,۰۰۰'],
+    ['img'=>'"images/phone-2.jpg"','product-title'=>' کاور جوی روم مدل JR-BP273 مناسب برای گوشی موبایل اپل iPhone 6 Plus', 'old-price'=>'۹۹,۰۰۰ ',
+        'discount'=>' ٪۵۴', 'new-price'=>'۴۵,۵۰۰ '],
+    ['img'=>'"images/phone-3.jpg"','product-title'=>' کاور جوی روم مدل JR-BP273 مناسب برای گوشی موبایل اپل iPhone 6 Plus', 'old-price'=>'۹۹,۰۰۰ ',
+        'discount'=>' ٪۶', 'new-price'=>'۴۵,۵۰۰ '],
+    ['img'=>'"images/phone-4.jpg"','product-title'=>'کاور نیلکین مدل Camshield مناسب برای گوشی موبایل شیائومی POCO X3 NFC', 'old-price'=>'۹۹,۰۰۰ ',
+        'discount'=>'٪۳۰ ', 'new-price'=>'۲۰۴,۷۹۰ '],
+    ['img'=>'"images/phone-5.jpg"','product-title'=>' کاور جوی روم مدل JR-BP273 مناسب برای گوشی موبایل اپل iPhone 6 Plus', 'old-price'=>'۹۹,۰۰۰ ',
+        'discount'=>' ٪۵۴', 'new-price'=>'۴۵,۵۰۰ '],
+    ['img'=>'"images/phone-6.jpg"','product-title'=>' کاور جوی روم مدل JR-BP273 مناسب برای گوشی موبایل اپل iPhone 6 Plus', 'old-price'=>'۹۹,۰۰۰ ',
+        'discount'=>' ٪۵۴', 'new-price'=>'۴۵,۵۰۰ '],
+    ['img'=>'"images/phone-7.jpg"','product-title'=>' کاور جوی روم مدل JR-BP273 مناسب برای گوشی موبایل اپل iPhone 6 Plus', 'old-price'=>'۹۹,۰۰۰ ',
+        'discount'=>' ٪۳۰', 'new-price'=>'۴۵,۵۰۰ '],
+    ['img'=>'"images/phone-8.jpg"','product-title'=>'کاور جویروم مدل Chi مناسب برای گوشی موبایل اپل iPhone 7 Plus', 'old-price'=>'۳۷,۰۰۰  ',
+        'discount'=>' ٪۵۴', 'new-price'=>'۲۹,۵۰۰ ']
+]
+?>
+@section('digi-plus')
+    <li class="nav-link  border-right-add">
+        <a href="#supermarket" class="nav-link-title">
+            <img src="images/digiplus-minicon.svg" class="w-4 h-4 ml-1 inline-block" alt="digiplus mini icon"> دیجی پلاس
+        </a>
+    </li>
+@endsection
 <div class="relative">
-    <div class="mobile-menu-container hide absolute top-0 right-0" id="mobile-menu-container"></div>
+    <div class="mobile-menu-container hide absolute top-0 right-0" id="mobile-menu-container">
+        <div class="content-container flex flex-col p-6">
+            <div class="mobile-menu-icon mb-3">
+                <div class="digikala-icon-container">
+                    <a href="digikala">
+                        <img src="images/digikala.svg" alt="digikala">
+                    </a>
+                </div>
+            </div>
+            <div class="mobile-menu-items mb-3 list-none">
+                @foreach($sandwichMenuItemsP1 as $sandwichmenuitemP1)
+
+                    <li class="nav-link  border-right-add">
+                        <a href="#supermarket" class="nav-link-title">
+                            <i class={!! $sandwichmenuitemP1['class'] !!}></i>{{$sandwichmenuitemP1['title']}}
+                        </a>
+                    </li>
+                @endforeach
+                @section('digi-plus')
+                    @show
+                    @foreach($sandwichMenuItemsP2 as $sandwichmenuitemP2)
+
+                        <li class="nav-link  border-right-add">
+                            <a href="#title" class="nav-link-title">
+                                <i class={!! $sandwichmenuitemP2['class'] !!}></i>{{$sandwichmenuitemP2['title']}}
+                            </a>
+                        </li>
+                    @endforeach
+            </div>
+            <div class="mobile-menu-cats mb-3"></div>
+        </div>
+    </div>
 
 @section('mobileSecTitle')
         <span class="text-gray-500  mobile-title	">برندهای محبوب</span>
@@ -1109,27 +1181,7 @@
                     <div class="specials">
                         <div class="product-items ">
                             <!--product item component-->
-                            <?php
-                            $productItemContainer3 = [
-                                ['img'=>'"images/phone-1.jpg"','product-title'=>'گوشی موبایل شیائومی مدل POCO X3 M2007J20CG دو سیم‌ کارت ظرفیت 128 گیگابایت
-                                            گوشی موبایل شیائومی مدل POCO X3 M2007J20CG دو سیم‌ کارت ظرفیت 128 گیگابایت', 'old-price'=>'۸,۰۰۰,۰۰۰',
-                                    'discount'=>' ۱۵٪', 'new-price'=>'۷,۲۲۹,۰۰۰'],
-                                ['img'=>'"images/phone-2.jpg"','product-title'=>' کاور جوی روم مدل JR-BP273 مناسب برای گوشی موبایل اپل iPhone 6 Plus', 'old-price'=>'۹۹,۰۰۰ ',
-                                    'discount'=>' ٪۵۴', 'new-price'=>'۴۵,۵۰۰ '],
-                                ['img'=>'"images/phone-3.jpg"','product-title'=>' کاور جوی روم مدل JR-BP273 مناسب برای گوشی موبایل اپل iPhone 6 Plus', 'old-price'=>'۹۹,۰۰۰ ',
-                                    'discount'=>' ٪۶', 'new-price'=>'۴۵,۵۰۰ '],
-                                ['img'=>'"images/phone-4.jpg"','product-title'=>'کاور نیلکین مدل Camshield مناسب برای گوشی موبایل شیائومی POCO X3 NFC', 'old-price'=>'۹۹,۰۰۰ ',
-                                    'discount'=>'٪۳۰ ', 'new-price'=>'۲۰۴,۷۹۰ '],
-                                ['img'=>'"images/phone-5.jpg"','product-title'=>' کاور جوی روم مدل JR-BP273 مناسب برای گوشی موبایل اپل iPhone 6 Plus', 'old-price'=>'۹۹,۰۰۰ ',
-                                    'discount'=>' ٪۵۴', 'new-price'=>'۴۵,۵۰۰ '],
-                                ['img'=>'"images/phone-6.jpg"','product-title'=>' کاور جوی روم مدل JR-BP273 مناسب برای گوشی موبایل اپل iPhone 6 Plus', 'old-price'=>'۹۹,۰۰۰ ',
-                                    'discount'=>' ٪۵۴', 'new-price'=>'۴۵,۵۰۰ '],
-                                ['img'=>'"images/phone-7.jpg"','product-title'=>' کاور جوی روم مدل JR-BP273 مناسب برای گوشی موبایل اپل iPhone 6 Plus', 'old-price'=>'۹۹,۰۰۰ ',
-                                    'discount'=>' ٪۳۰', 'new-price'=>'۴۵,۵۰۰ '],
-                                ['img'=>'"images/phone-8.jpg"','product-title'=>'کاور جویروم مدل Chi مناسب برای گوشی موبایل اپل iPhone 7 Plus', 'old-price'=>'۳۷,۰۰۰  ',
-                                    'discount'=>' ٪۵۴', 'new-price'=>'۲۹,۵۰۰ ']
-                            ]
-                            ?>
+
                             @foreach($productItemContainer3 as $productItem3 )
                                 <div class="product-item-container-3 rounded-2xl">
                                     <div class="product-item">
