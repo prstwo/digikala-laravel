ff7b038bfb66c884d78a6a442b680b65856fa577
unknown <prs2.pari@gmail.com>
2021-04-26 10:10:49 +0430

    product page added

---
+++ resources/views/single-product.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>مشخصات، قیمت و خرید گوشی موبایل </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/nav-menu.css">
+    <link rel="stylesheet" type="text/css" href="css/bullets-slider.css">
+    <link type="text/css" href="css/product-item.css" rel="stylesheet" >
+    <link rel="stylesheet" type="text/css" href="css/carousel.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/footer.css">
+
+</head>
+<body>
+<div>
+    <!--در این قسمت هدر و نوبار جای دارند -->
+    <main class="w-full">
+        <div class="product-whole-content mt-4">
+            <div class="container px-6 py-0 bg-white">
+                <div class="product-breadcrumb"></div>
+                <article>
+                    <section class="product-gallery"></section>
+                    <section class="product-info"></section>
+                </article>
+                <div class="product-suppliers">
+                    <div class="box-suppliers-head"></div>
+                    <div class="suppliers-list"></div>
+                </div>
+                <div class="carousel-horizontal-general"></div>
+                <div class="product-more">
+                    <div>
+                        <ul class="box-tabs-sticky"></ul>
+                        <div class="box-tabs-info">
+                            <div class="product-expert"></div>
+                            <div class="product-params"></div>
+                            <div class="product-comments"></div>
+                            <div class="product-faq"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
+
+    <script src="js/nav-menu.js"></script>
+    <script src="js/category-products.js"></script>
+    <script src="js/footer.js"></script>
+</body>
+</html>
