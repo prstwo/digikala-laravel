1f6444d3b7aba9efa20feffda3070f56f38a8556
unknown <prs2.pari@gmail.com>
2021-04-26 13:15:50 +0430

    article section of page is almost completed

--- resources/views/single-product.blade.php
+++ resources/views/single-product.blade.php
@@ -12,6 +12,18 @@
 
 </head>
 <body>
+<?php
+ $productConfigInfo = [
+         ['span1'=>'حافظه داخلی:', 'span2'=>'256 گیگابایت'],
+         ['span1'=>'شبکه های ارتباطی: ', 'span2'=>'2G, 3G, 4g, 5G '],
+         ['span1'=>'دوربین‌های پشت گوشی: ', 'span2'=>' 3 ماژول دوربین'],
+         ['span1'=>'توضیحات سیم کارت: ', 'span2'=>' سایز نانو (8.8 × 12.3 میلی‌متر)'],
+        ['span1'=>'حافظه داخلی:', 'span2'=>'256 گیگابایت'],
+        ['span1'=>'شبکه های ارتباطی: ', 'span2'=>'2G, 3G, 4g, 5G '],
+        ['span1'=>'دوربین‌های پشت گوشی: ', 'span2'=>' 3 ماژول دوربین'],
+        ['span1'=>'توضیحات سیم کارت: ', 'span2'=>' سایز نانو (8.8 × 12.3 میلی‌متر)'],
+    ]
+?>
 <div>
     <!--در این قسمت هدر و نوبار جای دارند -->
     <main class="w-full">
@@ -45,9 +57,109 @@
                         </a>
                     </div>
                 </div>
-                <article>
-                    <section class="product-gallery"></section>
-                    <section class="product-info"></section>
+                <article class="bg-white w-full flex flex-row mb-3 items-start">
+                    <section class="product-gallery pl-4 ml-4 flex flex-col top-28">
+                        <div class="product-gallery-item">
+                            <ul class="product-gallery-option list-none flex flex-col "></ul>
+                            <div></div>
+                        </div>
+                        <div class="product-gallery-main-pic">
+                            <img src="images/product-main-pic.jpg" alt="product pic">
+                        </div>
+                    </section>
+                    <section class="product-info">
+                        <div class="product-info-title flex px-0 py-2 items-center">
+                                <div>
+                                    <div class="product-title-flex">
+                                        <div class="product-title-brand flex items-center">
+                                            <a class="product-title-brand-link" href="#شیاُومی">
+
+                                                شیائومی
+                                            </a>
+                                            <span> /
+                                            </span>
+                                            <a class="product-title-brand-link" href="#شیاُومی">
+                                                گوشی موبایل شیائومی
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <h1 class="single-product-title ">
+                                        گوشی موبایل شیائومی مدل Mi 10T PRO 5G M 2007J3SG دو سیم&zwnj; کارت ظرفیت 256 گیگابایت
+                                    </h1>
+                                </div>
+                        </div>
+                        <div class="product-attributes flex flex-row">
+                            <div class="product-config flex-shrink-0 relative pt-3 ml-4 border-t border-solid flex flex-col content-between ">
+                                <span class="product-title-english relative bottom-6 right-0 pl-2 bg-white">Xiaomi Mi 10T PRO 5G M 2007J3SG Dual SIM 256GB Mobile Phone</span>
+                                <div class="product-engagement">
+                                    <div class="flex items-center">
+                                        <div class="product-engagement-rating flex items-center">
+                                            <span class="product-engagement-rating-star fa fa-star  ml-1"></span>
+                                            ۴.۲
+                                            <span class="product-engagement-rating-num mr-0.5 text-xs">
+                                            (۶۵)
+                                            </span>
+
+                                        </div>
+                                    </div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                                <div class="product-config-wrapper">
+                                    <div class="product-config-info">
+                                        <ul class="list-none p-0 my-5 mx-0">
+                                            @foreach($productConfigInfo as $productlist)
+                                                <li>
+                                                    <span>{{$productlist['span1']}} </span>
+                                                    <span>{{$productlist['span2']}} </span>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="product-summary">
+                                <div class="product-seller-info border border-solid rounded-lg">
+                                    <div class="product-seller-box relative flex flex-col">
+                                        <div class="product-seller-counter flex items-center">
+                                            <div>فروشنده</div>
+                                            <a href="#suppliers" class="seller-count-row">
+                                                <span class="font-bold">۴</span>
+                                                <span class="font-bold"> فروشنده</span>
+                                                دیگر
+                                            </a>
+                                        </div>
+                                        <div class="product-seller-row">
+                                            <div class="product-seller-row-main">
+                                                <div class="product-seller-first-line p-4 flex justify-between">
+                                                    <span>دیجی‌کالا</span>
+                                                    @section('angle-left')
+                                                        <i class="fa fa-angle-left "></i>
+                                                    @endsection
+                                                    @section('angle-left')
+                                                    @show
+                                                </div>
+                                                <div class="product-seller-second-line"></div>
+
+                                            </div>
+                                        </div>
+                                        <div class="product-seller-row p-4">
+                                            <div class="product-seller-row-main">
+                                                گارانتی ۱۸ ماهه کاوش تیم
+                                            </div>
+                                        </div>
+                                        <div class="product-seller-row p-4">
+                                            <div class="productdelivery-warehouse relative pr-10 mt-5">
+                                            </div>
+                                        </div>
+                                        <div class="product-seller-row p-4"> </div>
+                                    </div>
+                                    <div class="product-seller-add-to-card"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </article>
                 <div class="product-suppliers">
                     <div class="box-suppliers-head"></div>
