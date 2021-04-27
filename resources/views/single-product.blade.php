c5ac785b4a4a0f718ed4ef2380685607c3b69828
unknown <prs2.pari@gmail.com>
2021-04-27 12:33:51 +0430

    product-suppliers section is added

--- resources/views/single-product.blade.php
+++ resources/views/single-product.blade.php
@@ -210,9 +210,71 @@
                         </div>
                     </section>
                 </article>
+                <!--بهتره بخش با کلاس head رو بعد تبدیل به کامپوننت کنم -->
                 <div class="product-suppliers">
-                    <div class="box-suppliers-head"></div>
-                    <div class="suppliers-list"></div>
+                    <div class="box-suppliers-head">
+                        <div class="head">
+                            <span class="text-gray-500  mobile-title	">لیست فروشندگان این کالا</span>
+                            <span class="head-title">
+لیست فروشندگان این کالا                        </span>
+                            <div class="head-link">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="suppliers-list">
+                        <!-- -->
+                        <div class="supplier-list-row flex flex-wrap justify-between" >
+                            <div class="supplier-cell supplier-cell-title">
+                                <span class="supplier-seller-icon   is-digikala"></span>
+                                <div class="suppliers-seller-wrapper relative">
+                                    <p class="suppliers-seller-wrapper-name">
+                                        <a href="#kala-supplier">
+                                            دیجی&zwnj;کالا
+                                        </a>
+                                    </p>
+                                    <div class="suppliers-rating">
+                                        عملکرد:
+                                        <span class="u-text-bold">
+                                            ۵
+                                        </span>
+                                        از ۵
+                                        <span class="u-divider"></span>
+                                        <span class="u-text-bold">
+                                            ۹۳.۴٪
+                                        </span>
+                                        رضایت از کالا
+                                    </div>
+                                    <p>
+
+                                    </p>
+                                </div>
+
+                            </div>
+                            <div class="supplier-cell supplier-cell-conditions">
+                                <div class="suppliers-sender suppliers-sender-digikala no-lead-time">
+                                    ارسال دیجی&zwnj;کالا
+                                </div></div>
+                            <div class="supplier-cell supplier-cell-guarantee">
+                                <span>گارانتی ۱۸ ماهه کاوش تیم</span>
+                            </div>
+                            <div class="supplier-cell suppliers-cell-price ">
+                                <div class="c-price">
+                                    <div class="c-price-value">
+                                        ۱۳,۳۹۹,۰۰۰
+                                        <span class="text-sm">تومان</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="supplier-cell  supplier-cell-action">
+                                <a class=" o-btn o-btn-outlined-red " href="#add-to-card">
+                                    افزودن به سبد
+                                </a>
+                            </div>
+                        </div>
+
+                        <!-- -->
+                    </div>
                 </div>
                 <div class="carousel-horizontal-general"></div>
                 <div class="product-more">
