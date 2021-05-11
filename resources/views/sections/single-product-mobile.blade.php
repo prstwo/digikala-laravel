fba49a2d1db76c5b5bb39a018eccb65a17d9c158
unknown <prs2.pari@gmail.com>
2021-05-11 12:42:47 +0430

    samane hamta section is added

--- resources/views/sections/single-product-mobile.blade.php
+++ resources/views/sections/single-product-mobile.blade.php
@@ -86,7 +86,21 @@
             </div>
 
         </section>
-        <section class="my-4"></section>
+        <section class="my-4">
+            <div class="c-product-data ">
+                <div class="c-product-data-section-additional ">
+                    <div class="c-product-data-info-icon"></div>
+                    <span>
+                        هشدار
+                        سامانه همتا: حتما در زمان تحویل دستگاه، به کمک کد فعال&zwnj;سازی چاپ شده
+                        روی جعبه یا کارت
+                        گارانتی، دستگاه را
+                        از طریق #7777*،
+                        برای سیم&zwnj;کارت خود فعال&zwnj;سازی کنید. آموزش تصویری در آدرس اینترنتی hmti.ir/05
+                    </span>
+                </div>
+            </div>
+        </section>
         <div class="c-checkout-submit flex-col">
             <a href="#kharid" class="c-btn-action">
                 افزودن به سبد خرید
