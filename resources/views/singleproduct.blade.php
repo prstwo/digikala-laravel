8cfd0784ea3f5190cbf1f2343beb11a8b4533c60
unknown <prs2.pari@gmail.com>
2021-05-16 17:13:48 +0430

    samane hamta section is added to desktop version

--- resources/views/singleproduct.blade.php
+++ resources/views/singleproduct.blade.php
@@ -111,6 +111,19 @@
                                                 + موارد بیشتر
                                             </a>
                                         </div>
+                                        <div class="mb-5">
+                                            <div class="c-product-data-section-additional  desktop-hamta">
+                                                <div class="c-product-data-info-icon"></div>
+                                                    <span>
+                                                        هشدار
+                                                        سامانه همتا: حتما در زمان تحویل دستگاه، به کمک کد فعال&zwnj;سازی چاپ شده
+                                                        روی جعبه یا کارت
+                                                        گارانتی، دستگاه را
+                                                        از طریق #7777*،
+                                                        برای سیم&zwnj;کارت خود فعال&zwnj;سازی کنید. آموزش تصویری در آدرس اینترنتی hmti.ir/05
+                                                    </span>
+                                            </div>
+                                        </div>
                                     </div>
 
                                 </div>
